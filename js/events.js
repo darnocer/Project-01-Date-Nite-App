28e71bcc838c5b5cf6bcfa8b63ac94e75b5bbde6
darnocer <darnocer@gmail.com>
2020-05-02 11:18:28 -0600

    linking scripts, ticketmaster api

--- js/events.js
+++ js/events.js
@@ -1,8 +1,17 @@
 function getEvent() {
+  var APIKeyEvents = Ds8xghq3sGbqB3ntRWxevADM1AJPkuzm;
+  var city = denver;
+  var date = "2020-08-08";
+
   $.ajax({
     type: "GET",
     url:
-      "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Ds8xghq3sGbqB3ntRWxevADM1AJPkuzm",
+      "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" +
+      APIKeyEvents +
+      "&city=" +
+      city +
+      "&startDateTime=" +
+      date,
     async: true,
     dataType: "json",
     success: function (json) {
