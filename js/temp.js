c09717238358628dc159e808929c14456d04208f
darnocer <darnocer@gmail.com>
2020-05-05 20:33:56 -0600

    updating weather info

--- js/temp.js
+++ js/temp.js
@@ -3,8 +3,21 @@ $(document).ready(function () {
   // var userCity = $("#city-input").val();
   // var userDate = $("#date-input").val();
 
-  var userCity = "denver";
-  var userDate = "2020-05-06";
+  $("#submit-button").on("click", function () {
+    event.preventDefault();
+    console.log("button was clicked");
+    $("#user-inputs").addClass("is-hidden");
+    $("#date-results").removeClass("is-hidden");
+    userCity = $("#city-input").val();
+    userDate = $("#date-input").val();
+    getWeather();
+    formatDate();
+    console.log(cityInput);
+    console.log("user date: " + userDate);
+  });
+
+  // var userCity = "denver";
+  // var userDate = "2020-05-06";
   var weather;
 
   function getWeather() {
@@ -21,27 +34,35 @@ $(document).ready(function () {
       var results = response.list;
       console.log(results);
 
-      var date;
+      var forecastDate;
+
+      $("#temp").html("");
 
       for (i = 0; i < results.length; i++) {
-        date = results[i].dt_txt.slice(0, 10);
-        if (date === userDate) {
+        forecastDate = results[i].dt_txt.slice(0, 10);
+        if (forecastDate === userDate) {
           var tempK = results[i].main.temp;
           weather = results[i].weather[0].main;
           break;
         }
       }
 
-      var tempF = (tempK - 273.15) * 1.8 + 32;
+      if (tempK !== undefined) {
+        var tempF = (tempK - 273.15) * 1.8 + 32;
 
-      $("#temp").html("Temperature: " + tempF.toFixed(1) + " &deg;" + "F");
+        $("#temp").html("Temperature: " + tempF.toFixed(1) + " &deg;" + "F");
 
-      $("#weather-icon").html("");
-      renderIcons();
-      weatherIcon = $("<img>").attr("src", imgURL);
-      weatherIcon.attr("alt", "weather icon");
+        $("#weather-icon").html("");
+        renderIcons();
+        weatherIcon = $("<img>").attr("src", imgURL);
+        weatherIcon.attr("alt", "weather icon");
 
-      $("#weather-icon").append(weatherIcon);
+        $("#weather-icon").append(weatherIcon);
+      } else {
+        var unavailable = $("<p>").addClass("unavailable");
+        unavailable.text("Weather data not available");
+        $("#temp").append(unavailable);
+      }
     });
   }
 
@@ -78,6 +99,5 @@ $(document).ready(function () {
     imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
   }
 
-  getWeather();
-  formatDate();
+  // getWeather();
 });
